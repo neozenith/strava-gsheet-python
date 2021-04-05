b5d2a14b24c72e3d0a67ed4fca82748073c881e6
Josh Peak <neozenith.dev@gmail.com>
2021-04-05 21:38:15 +1000

    Add POST webhook route

--- api/v1.py
+++ api/v1.py
@@ -1,12 +1,23 @@
 """Main API entrypoint."""
 
+# Standard Library
+from pprint import pprint as pp
+
 # Third Party Libraries
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def status():
     """Root handler."""
     return {"message": "Hello World"}
+
+
+@app.post("/")
+async def webhook(event: Request):
+    """Webhook accepting any JSON payload and just returning it."""
+    jsonData = await event.json()
+    pp(jsonData)
+    return jsonData
--- tests/test_api_v1.py
+++ tests/test_api_v1.py
@@ -7,7 +7,19 @@ from api.v1 import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_status():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_post_webhook():
+    response = client.post(
+        "/",
+        headers={},
+        json={"id": "foobar"},
+    )
+    #  assert response.status_code == 200
+    assert response.json() == {
+        "id": "foobar",
+    }
