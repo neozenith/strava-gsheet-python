0d5135ffd174570f8246a7fd0838fba28746b859
Josh Peak <neozenith.dev@gmail.com>
2021-04-09 16:33:10 +1000

    Save activities to Mongo database. (#4)

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,25 +1,69 @@
 """Command Line Tool for automating tasks to transfer data between Strava and GSheets."""
 
 # Standard Library
+import inspect
 import os
 import sys
-from pprint import pprint
+from pprint import pprint as pp
 
 # Third Party Libraries
 from dotenv import load_dotenv
 
 # Our Libraries
+from core.db import Database
 from core.strava import StravaAPIWrapper
 
 load_dotenv()
 
 
-def main(args):
-    """CLI entry point."""
-    print(args)
-    strava = StravaAPIWrapper(os.getenv("STRAVA_ACCESS_TOKEN"))
-    pprint(strava.list_activities(perPage=5))
+def _inspect_tasks(prefix):
+    return {
+        f[0].replace(prefix, ""): f[1]
+        for f in inspect.getmembers(sys.modules["__main__"], inspect.isfunction)
+        if f[0].startswith(prefix)
+    }
+
+
+def cmd_extract(args):
+    """Task to extract Strava SummaryActivities and save to database."""
+    strava = StravaAPIWrapper(
+        os.getenv("STRAVA_CLIENT_ID"),
+        os.getenv("STRAVA_CLIENT_SECRET"),
+        os.getenv("STRAVA_ACCESS_TOKEN"),
+        os.getenv("STRAVA_REFRESH_TOKEN"),
+    )
+    db = Database(os.getenv("MONGO_CONNECTION_STRING"))
+
+    activities = strava.list_activities(perPage=5)
+    filtered = [_filtered_activity(a) for a in activities]
+    result = db.save_activities(filtered)
+    pp(result)
+
+
+def _filtered_activity(activity):
+    target_attributes = [
+        "id",
+        "name",
+        "start_date_local",
+        "moving_time",
+        "elapsed_time",
+        "type",
+        "workout_type" "distance",
+        "total_elevation_gain",
+        "kilojoules",
+        "average_speed",
+        "max_speed",
+        "average_watts",
+        "max_watts",
+        "weighted_average_watts",
+    ]
+    return {attr: getattr(activity, attr) for attr in activity.__dir__() if attr in target_attributes}
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    tasks = _inspect_tasks("cmd_")
+
+    if len(sys.argv) >= 2 and sys.argv[1] in tasks.keys():
+        tasks[sys.argv[1]](sys.argv[2:])
+    else:
+        print(f"Must provide a task from the following: {list(tasks.keys())}")
--- core/db.py
+++ core/db.py
@@ -14,3 +14,9 @@ class Database:
         """Initialize Database client with a connection string."""
         super().__init__()
         self.client = MongoClient(connection_string)
+
+    def save_activities(self, activities):
+        """Save list of Strava Activities to mongo activities collection."""
+        db = self.client["workouttracker"]
+        collection = db["activities"]
+        return collection.insert_many(activities)
