af67c34b784341e77d2fa9dfcfe8c9d8325de4cf
Josh Peak <neozenith.dev@gmail.com>
2021-04-09 17:58:24 +1000

    Add API date filtering (#5)
    - Added API date filtering
     - Added relative date specifiers
     - paginate through all activities for specified date range

--- cli/__main__.py
+++ cli/__main__.py
@@ -4,6 +4,7 @@
 import inspect
 import os
 import sys
+import time
 from pprint import pprint as pp
 
 # Third Party Libraries
@@ -34,30 +35,40 @@ def cmd_extract(args):
     )
     db = Database(os.getenv("MONGO_CONNECTION_STRING"))
 
-    activities = strava.list_activities(perPage=5)
-    filtered = [_filtered_activity(a) for a in activities]
-    result = db.save_activities(filtered)
+    valid_args = ["before", "after", "page", "per_page", "before_days_ago", "after_days_ago"]
+    kwargs = validate_args(args, valid_args)
+
+    # Allow relative date args to specify the exact epoch times.
+    if "after_days_ago" in kwargs:
+        kwargs["after"] = int(time.time()) - int(kwargs["after_days_ago"]) * 24 * 60 * 60
+        del kwargs["after_days_ago"]
+
+    if "before_days_ago" in kwargs:
+        kwargs["before"] = int(time.time()) - int(kwargs["before_days_ago"]) * 24 * 60 * 60
+        del kwargs["before_days_ago"]
+
+    page = 1
+    activities = [{"dummy": "bootstrap"}]
+    total = 0
+    while len(activities) > 0:
+        activities = strava.list_activities(page=page, **kwargs)
+        print(f"page:{page} [{len(activities)}]")
+        total = total + len(activities)
+        page = page + 1
+    print(f"TOTAL: {total}")
+
+    pp(activities)
+    result = db.save_activities(activities)
     pp(result)
 
 
-def _filtered_activity(activity):
-    target_attributes = [
-        "id",
-        "name",
-        "start_date_local",
-        "moving_time",
-        "elapsed_time",
-        "type",
-        "workout_type" "distance",
-        "total_elevation_gain",
-        "kilojoules",
-        "average_speed",
-        "max_speed",
-        "average_watts",
-        "max_watts",
-        "weighted_average_watts",
-    ]
-    return {attr: getattr(activity, attr) for attr in activity.__dir__() if attr in target_attributes}
+def validate_args(args, valid_args):
+    """Validate args in format '--key=value'."""
+    return {
+        arg.replace("--", "").split("=")[0]: arg.replace("--", "").split("=")[1]
+        for arg in args
+        if arg.startswith("--") and "=" in arg and arg.replace("--", "").split("=")[0] in valid_args
+    }
 
 
 if __name__ == "__main__":
--- core/strava.py
+++ core/strava.py
@@ -18,15 +18,36 @@ class StravaAPIWrapper:
         self.access_token = access_token
         self.refresh_token = refresh_token
 
-    def list_activities(self, page=1, perPage=30):
+    def list_activities(self, before="", after="", page=1, per_page=30):
         """Extract a list of athlete activities from Strava API."""
-        api_response = None
+        api_response = []
 
         try:
             api_instance = swagger_client.ActivitiesApi()
             api_instance.api_client.configuration.access_token = self.access_token
-            api_response = api_instance.get_logged_in_athlete_activities(page=page, per_page=perPage)
+            api_response = api_instance.get_logged_in_athlete_activities(
+                before=before, after=after, page=page, per_page=per_page
+            )
         except ApiException as e:
             print("Exception when calling ActivitiesApi->getLoggedInAthleteActivities: %s\n" % e)
 
-        return api_response
+        return [self._filtered_activity(a) for a in api_response]
+
+    def _filtered_activity(self, activity):
+        target_attributes = [
+            "id",
+            "name",
+            "start_date_local",
+            "moving_time",
+            "elapsed_time",
+            "type",
+            "workout_type" "distance",
+            "total_elevation_gain",
+            "kilojoules",
+            "average_speed",
+            "max_speed",
+            "average_watts",
+            "max_watts",
+            "weighted_average_watts",
+        ]
+        return {attr: getattr(activity, attr) for attr in activity.__dir__() if attr in target_attributes}
